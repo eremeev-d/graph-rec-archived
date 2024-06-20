cafeb9966909c8742c268fce267818f24feeb4fd
erermeev-d <eremeevd2002@yandex.ru>
2024-06-21 01:54:32 +0300

    add faiss index with simple als embeddings

--- app/data.py
+++ app/data.py
@@ -1,4 +1,18 @@
+import streamlit as st
 import pandas as pd
+import numpy as np
+import faiss
 
 
 items_data = pd.read_csv("../data/items.csv")
+
+
+@st.cache_data
+def get_embeddings():
+    return np.load("../data/embeddings.npy")
+
+
+@st.cache_resource()
+def get_faiss_index():
+    index = faiss.read_index("../data/index.faiss")
+    return index
--- app/recommendation.py
+++ app/recommendation.py
@@ -1,9 +1,15 @@
 import random
 
-from data import items_data
+import numpy as np
+
+from data import items_data, get_embeddings, get_faiss_index
 
 
-def recommend_items(rec_query, n_items=10):
-    random.seed(rec_query)
-    results = [random.randint(0, len(items_data)) for _ in range(n_items)]
-    return results
+def recommend_items(query, n_items=10):
+    # TODO: check that we dont spend too much time at this
+    embeddings = get_embeddings()
+    query_embedding = embeddings[query, :]
+    
+    index = get_faiss_index()
+    _, results = index.search(query_embedding[None, :], k=n_items)
+    return results[0]
